68f337cafef54cd5cbc7e4c1487dab92f3c82696
AntonBereza1995 <80589071+AntonBereza1995@users.noreply.github.com>
2021-11-26 13:50:40 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import Profile from './components/Profile';
-import user from './components/Profile/user.json';
-import Statistics from './components/Statistics';
-import statisticalData from './components/Statistics/statistical-data.json';
-import FriendList from './components/FriendList';
-import TransactionHistory from './components/TransactionHistory';
-import transactions from './components/TransactionHistory/transactions.json';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/Friendlist/FriendList';
+import TransactionHistory from './components/Transaction/TransactionHistory';
+import Container from './components/Container/Container';
+
+import user from "./data/user.json";
+import statisticalData from "./data/statistical-data.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
+
 function App() {
   return (
-    <div>
-      <Profile
-        avatar={user.avatar}
-        name={user.name}
+    <div className="App" style={{ backgroundColor: "rgb(216, 224, 234)"}}>
+      <Container><Profile         
+      name={user.name}
         tag={user.tag}
         location={user.location}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <FriendList />
-      <TransactionHistory items={transactions} />
+        avatar={user.avatar}
+        stats={user.stats}/></Container>
+      <Container><Statistics title="Upload stats" stats={statisticalData}/></Container>
+      <Container><FriendList friend={friends}/></Container>
+      <Container><TransactionHistory items={transactions}/></Container>
     </div>
   );
 }
